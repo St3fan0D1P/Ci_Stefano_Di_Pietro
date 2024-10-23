7f5d786a30c092b199a2a53519bc7c8d03597b5f
St3fan0D1P <stefanodipietro2204@gmail.com>
2024-10-23 23:17:55 +0200

    prima proma/commit

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.json({message: "Hello World!"});
+})
+
+app.get("/client", (req, res) => {
+    res.json({message: "Hello Client!"});
+})
+
+app.get("/pippo", (req, res) => {
+    res.json({message: "Hello Pippo!"});
+})
+
+const server = app.listen(port, () => {
+    console.log("Sono in ascolto")
+})
+
+const close = () => {
+    server.close();
+}
+
+module.exports = { app, close }
